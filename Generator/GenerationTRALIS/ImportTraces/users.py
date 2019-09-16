477d1696b86cb21e11dcf1ef2dcab285faa2abf7
Leonardo Sarmiento <43929324+Leo1690@users.noreply.github.com>
2019-09-16 11:53:30 +0200

    Delete users.py

--- Generator/GenerationTRALIS/ImportTraces/users.py
+++
@@ -1,9 +0,0 @@
-#objects to process and represent traces
-#User trace
-class User(object):
-    def __init__(self, time, x, y):
-        self.time = time;   #list of time stamps of trace-points
-        self.x = x;         #list of X coordinates of trace-points
-        self.y = y;         #list of Y coordinates of trace-points
-        self.inputPort = [None]*len(self.x);    #list of input locations
-        self.outputPort = [None]*len(self.x);   #list of output locations
